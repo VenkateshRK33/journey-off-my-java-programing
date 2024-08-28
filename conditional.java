98c976f921ac43837bb6f72158d18e762f02b9f5
VenkateshRK33 <venkateshrkulkarni1@gmail.com>
2024-08-29 00:00:31 +0530

    condtional statement are imp

---
+++ conditional.java
@@ -0,0 +1,19 @@
+import java.util.*;
+    
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("what's web-site do u wanna check ");
+    String website = sc.next();
+
+    if(website.endsWith(".org")){
+        System.out.println("it's organisation website");
+    }
+    else if(website.endsWith(".in")){
+        System.out.println("it's indian website");
+
+    }
+    else if(website.endsWith(".com")){
+        System.out.println("it's commerical website ");
+
+    }
+}
---
+++ sum.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public static void main(String[] args) {
+    System.out.println("taking users details");
+
+    Scanner obj = new Scanner(System.in);
+    System.out.println("enter ur num1");
+    int a=obj.nextInt();
+    System.out.println("enter ur num2");
+    int b=obj.nextInt();
+    int sum=a+b;
+    System.out.println("this is sum of integers :"+sum);
+}
