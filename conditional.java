45dfbb30768f889e3f3d2a2fa5c47cfa3fbb320a
VenkateshRK33 <venkateshrkulkarni1@gmail.com>
2024-11-15 21:32:49 +0530

    .

--- conditional.java
+++ conditional.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
     System.out.println("what's web-site do u wanna check ");
     String website = sc.next();
 
+    
     if(website.endsWith(".org")){
         System.out.println("it's organisation website");
     }
